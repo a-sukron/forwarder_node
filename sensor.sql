786415251a4ce8155815109e9aaaf7c380a3590f
a-sukron <115721495+a-sukron@users.noreply.github.com>
2022-10-13 22:17:53 +0700

    Add files via upload

---
+++ sensor.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 13, 2022 at 10:16 PM
+-- Server version: 10.3.36-MariaDB-0+deb10u2
+-- PHP Version: 7.3.31-1~deb10u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sensor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `value`
+--
+
+CREATE TABLE `value` (
+  `id` varchar(15) DEFAULT NULL,
+  `val` int(8) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `value`
+--
+
+INSERT INTO `value` (`id`, `val`, `timestamp`) VALUES
+('ESP32_001', 1420, '2022-10-13 15:16:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `value`
+--
+ALTER TABLE `value`
+  ADD UNIQUE KEY `sensor_id` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
